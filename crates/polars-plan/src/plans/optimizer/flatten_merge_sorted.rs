57590239ec214bd4f6a17ed972149700973f0a89
Koen van Wel <koen@lyfo.com>
2026-06-23 19:02:44 +0200

    Propagate key into `collect_merge_sorted_inputs`

--- crates/polars-plan/src/plans/optimizer/flatten_merge_sorted.rs
+++ crates/polars-plan/src/plans/optimizer/flatten_merge_sorted.rs
@@ -45,7 +45,7 @@ impl OptimizationRule for FlattenMergeSortedRule {
         self.collected_inputs.clear();
         collect_merge_sorted_inputs(
             node,
-            &key[0],
+            key.clone(),
             maintain_order,
             lp_arena,
             &mut self.collected_inputs,
@@ -58,7 +58,7 @@ impl OptimizationRule for FlattenMergeSortedRule {
 
         Ok(Some(build_merge_sorted_subtree(
             &self.collected_inputs,
-            &key[0],
+            key.clone(),
             maintain_order,
             lp_arena,
             &mut self.optimized_nodes,
@@ -68,7 +68,7 @@ impl OptimizationRule for FlattenMergeSortedRule {
 
 fn collect_merge_sorted_inputs(
     root: Node,
-    key: &PlSmallStr,
+    key: Vec<PlSmallStr>,
     maintain_order: bool,
     lp_arena: &Arena<IR>,
     out: &mut Vec<Node>,
@@ -84,7 +84,7 @@ fn collect_merge_sorted_inputs(
                 input_right,
                 key: merge_key,
                 maintain_order: merge_maintain_order,
-            } if merge_key[0] == key && *merge_maintain_order == maintain_order => {
+            } if *merge_key == key && *merge_maintain_order == maintain_order => {
                 traversal_stack.push(*input_right);
                 traversal_stack.push(*input_left);
             },
@@ -95,7 +95,7 @@ fn collect_merge_sorted_inputs(
 
 fn build_merge_sorted_subtree(
     inputs: &[Node],
-    key: &PlSmallStr,
+    key: Vec<PlSmallStr>,
     maintain_order: bool,
     lp_arena: &mut Arena<IR>,
     optimized_nodes: &mut PlHashSet<Node>,
@@ -105,14 +105,14 @@ fn build_merge_sorted_subtree(
     let split = inputs.len().div_ceil(2);
     let input_left = build_merge_sorted_subtree_node(
         &inputs[..split],
-        key,
+        key.clone(),
         maintain_order,
         lp_arena,
         optimized_nodes,
     );
     let input_right = build_merge_sorted_subtree_node(
         &inputs[split..],
-        key,
+        key.clone(),
         maintain_order,
         lp_arena,
         optimized_nodes,
@@ -120,14 +120,14 @@ fn build_merge_sorted_subtree(
     IR::MergeSorted {
         input_left,
         input_right,
-        key: vec![key.clone()],
+        key,
         maintain_order,
     }
 }
 
 fn build_merge_sorted_subtree_node(
     inputs: &[Node],
-    key: &PlSmallStr,
+    key: Vec<PlSmallStr>,
     maintain_order: bool,
     lp_arena: &mut Arena<IR>,
     optimized_nodes: &mut PlHashSet<Node>,
